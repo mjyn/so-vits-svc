ca98874c7d4d4189d76ffab8ab2571449d6e7df1
mjyn <mjyn@live.cn>
2023-02-09 21:17:51 +0800

    Update onnx_export.py

--- onnx_export.py
+++ onnx_export.py
@@ -12,11 +12,11 @@ from hubert import hubert_model_onnx
 
 def main(HubertExport,NetExport):
 
-    path = "NyaruTaffy"
+    path = "yuzu"
 
     if(HubertExport):
         device = torch.device("cuda")
-        hubert_soft = hubert_model_onnx.hubert_soft("hubert/model.pt")
+        hubert_soft = hubert_model_onnx.hubert_soft("hubert/hubert-soft-0d54a1f4.pt")
         test_input = torch.rand(1, 1, 16000)
         input_names = ["source"]
         output_names = ["embed"]
